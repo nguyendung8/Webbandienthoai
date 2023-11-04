6e6b2d90979ebac65e73f947b38163f5a6ca95b8
manhdung <dungli1221@gmail.com>
2023-11-04 15:51:33 +0700

    init1

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-    $conn = mysqli_connect('localhost','root','','webbanquanao') or die('connection failed');
+    $conn = mysqli_connect('localhost','root','','webbandienthoai') or die('connection failed');
 
 ?>
